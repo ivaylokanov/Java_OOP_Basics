c5ff6df9856459b328f23b7394b023ca70889f78
ivaylokanov <kanov.ivo@gmail.com>
2017-04-30 01:37:27 +0300

    Java

---
+++ a_definingClasses/b_definingClasses_exercise/src/f_rawData/Car.java
@@ -0,0 +1,35 @@
+package f_rawData;
+
+
+import java.util.Set;
+
+public class Car {
+    private String carModel;
+    private Engine engine;
+    private Cargo cargo;
+    private Set<Tire> tires;
+
+    public Car(String carModel, Engine engine, Cargo cargo, Set<Tire> tires) {
+        this.carModel = carModel;
+        this.engine = engine;
+        this.cargo = cargo;
+        this.tires = tires;
+    }
+
+    public Cargo getCargo() {
+        return this.cargo;
+    }
+
+    public Set<Tire> getTires() {
+        return this.tires;
+    }
+
+    public Engine getEngine() {
+        return this.engine;
+    }
+
+    @Override
+    public String toString() {
+        return this.carModel;
+    }
+}
---
+++ a_definingClasses/b_definingClasses_exercise/src/f_rawData/Cargo.java
@@ -0,0 +1,15 @@
+package f_rawData;
+
+public class Cargo {
+    private int cargoWeight;
+    private String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+
+    public String getCargoType() {
+        return this.cargoType;
+    }
+}
