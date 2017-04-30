8dd3a1e17bc2867b610c139fd781cbb0fc5e7734
ivaylokanov <kanov.ivo@gmail.com>
2017-04-30 19:27:54 +0300

    Java

---
+++ a_definingClasses/b_definingClasses_exercise/src/g_carSalesman/Engine.java
@@ -0,0 +1,40 @@
+package g_carSalesman;
+
+public class Engine {
+    private String engineModel;
+    private int power;
+    private String displacement;
+    private String efficiency;
+
+    public Engine(String engineModel, int power) {
+        this.engineModel = engineModel;
+        this.power = power;
+        this.efficiency = "n/a";
+        this.displacement = "n/a";
+
+    }
+
+    public void setDisplacement(String displacement) {
+        this.displacement = displacement;
+    }
+
+    public void setEfficiency(String efficiency) {
+        this.efficiency = efficiency;
+    }
+
+    protected int getPower() {
+        return this.power;
+    }
+
+    protected String getDisplacement() {
+        return this.displacement;
+    }
+
+    protected String getEfficiency() {
+        return this.efficiency;
+    }
+
+    protected String getEngineModel() {
+        return this.engineModel;
+    }
+}
