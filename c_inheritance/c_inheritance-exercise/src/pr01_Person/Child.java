5b83dfa484d55d14a0d32ea6a0c78824381d78d8
ivaylokanov <kanov.ivo@gmail.com>
2017-05-14 01:51:10 +0300

    Java

--- c_inheritance/c_inheritance-exercise/src/pr01_Person/Child.java
+++ c_inheritance/c_inheritance-exercise/src/pr01_Person/Child.java
@@ -1,4 +1,15 @@
 package pr01_Person;
 
-public class Child {
+public class Child extends Person {
+    public Child(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void setAge(int age) {
+        if (age>15){
+            throw new IllegalArgumentException("Child's age must be lesser than 15!");
+        }
+        super.setAge(age);
+    }
 }
---
+++ c_inheritance/c_inheritance-exercise/src/pr02_bookShop/GoldenEditionBook.java
@@ -0,0 +1,14 @@
+package pr02_bookShop;
+
+public class GoldenEditionBook extends Book {
+
+
+    public GoldenEditionBook(String author, String title, double price) {
+        super(author, title, price);
+    }
+
+    @Override
+    protected double getPrice() {
+        return super.getPrice()+super.getPrice()*0.3;
+    }
+}
